5aa86cde4fa90ac544386a0d62b426fc077543f9
Tony Shi <tuo.shi@oracle.com>
2017-03-03 16:20:23 +0800

    change API to be implemented with Jersey

---
+++ src/main/java/net/nemo/whatever/api/ApiApplication.java
@@ -0,0 +1,23 @@
+package net.nemo.whatever.api;
+
+import net.nemo.whatever.api.filter.CORSResponseFilter;
+import net.nemo.whatever.api.resources.TodosResource;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+
+/**
+ * Created by tonyshi on 2017/3/3.
+ */
+public class ApiApplication extends ResourceConfig {
+
+
+    public ApiApplication(){
+        register(TodosResource.class);
+
+        register(RequestContextFilter.class);
+        register(CORSResponseFilter.class);
+
+        register(JacksonFeature.class);
+    }
+}
